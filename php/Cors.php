bdc376f136cd58cbebaa83a698712dbab9816987
amandamula <amandamulariera@hotmail.com>
2021-02-15 10:35:30 +0100

    Cors

--- php/Cors.php
+++ php/Cors.php
@@ -1,4 +1,4 @@
-<?php header("Access-Control-Allow-Origin: admin.mallorcarustic.me");
+<?php header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Credentials: true");
 
