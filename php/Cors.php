d2dd43085918d96c5ca8a4697590403c09245553
amandamula <amandamulariera@hotmail.com>
2021-02-15 10:46:25 +0100

    Cors

--- php/Cors.php
+++ php/Cors.php
@@ -1,4 +1,5 @@
 <?php header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Credentials: true");
 
