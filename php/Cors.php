be2a49083a2067a5f31965906448eed33273dea5
lauramanerasanso <lauraams99@gmail.com>
2021-02-23 13:31:35 +0100

    cors

--- php/Cors.php
+++ php/Cors.php
@@ -1,7 +1,8 @@
-<?php header("Access-Control-Allow-Origin: http://admin.mallorcarustic.me");
-header("Access-Control-Allow-Origin: http://www.mallorcarustic.me");
-header("Access-Control-Allow-Origin: https://admin.mallorcarustic.me");
-header("Access-Control-Allow-Origin: https://www.mallorcarustic.me");
+<?php 
+$allowed = ["http://admin.mallorcarustic.me", "https://admin.mallorcarustic.me","http://www.mallorcarustic.me", "https://www.mallorcarustic.me"];
+if(in_array($_SERVER['HTTP_ORIGIN'], $allowed)){
+    header("Access-Control-Allow-Origin: ".$_SERVER['HTTP_ORIGIN']);
+}
 header("Access-Control-Allow-Headers: X-API-KEY, X-Requested-With, Accept, Access-Control-Request-Method");
 header("Access-Control-Allow-Credentials: true");
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
