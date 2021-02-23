d2c12f41e24a27b2beed7e2fe524cbf1a8eb1dcb
lauramanerasanso <lauraams99@gmail.com>
2021-02-23 12:43:14 +0100

    cors https

--- php/Cors.php
+++ php/Cors.php
@@ -1,5 +1,7 @@
 <?php header("Access-Control-Allow-Origin: http://admin.mallorcarustic.me");
 header("Access-Control-Allow-Origin: http://www.mallorcarustic.me");
+header("Access-Control-Allow-Origin: https://admin.mallorcarustic.me");
+header("Access-Control-Allow-Origin: https://www.mallorcarustic.me");
 header("Access-Control-Allow-Headers: X-API-KEY, X-Requested-With, Accept, Access-Control-Request-Method");
 header("Access-Control-Allow-Credentials: true");
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
